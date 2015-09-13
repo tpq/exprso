5efb786a3b3a45aa2a994ae27e82166b4245af84
Thom Quinn <contacttomquinn@gmail.com>
2015-09-13 15:23:52 -0400

    Create install.R

---
+++ Before-using-Exprso-you-must-install-the-following-packages-source-https/bioconductor.org/install.R
@@ -0,0 +1,23 @@
+# Before using Exprso, you must install the following packages:
+
+source("https://bioconductor.org/biocLite.R")
+biocLite("Biobase")
+biocLite("limma")
+
+install.packages(c("plyr", "lattice", "sampling", "Matching", "penalizedSVM", "pathClass", "mRMRe", "e1071", "nnet", "ROCR"))
+
+# Check installed packages
+require(plyr)
+require(lattice)
+require(Biobase)
+require(sampling)
+require(Matching)
+require(penalizedSVM)
+require(pathClass)
+require(limma)
+require(mRMRe)
+require(e1071)
+require(nnet)
+require(ROCR)
+
+#.
