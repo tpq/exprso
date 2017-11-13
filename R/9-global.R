ea89f9dcaa51627b13a1206d84000bff8d8e0582
Thom Quinn <contacttomquinn@gmail.com>
2017-11-14 09:12:21 +1100

    Add packageCheck and classCheck back-end functions

--- R/9-global.R
+++ R/9-global.R
@@ -5,6 +5,34 @@
 #' @importFrom graphics plot
 NULL
 
+#' Package Check
+#'
+#' Checks whether the user has the required package installed.
+#'  For back-end use only.
+#'
+#' @param package A character string. An R package.
+packageCheck <- function(package){
+
+  if(!requireNamespace(package, quietly = TRUE)){
+    stop("Uh oh! This propr method depends on ", package, "!")
+  }
+}
+
+#' Class Check
+#'
+#' Checks whether an object belongs to a specified class.
+#'  For back-end use only.
+#'
+#' @param x An object.
+#' @param what A character vector. The classes any of which \code{x} should have.
+#' @param msg A string. An error message if \code{x} is not \code{what}.
+checkClass <- function(x, what){
+
+  if(class(x) %in% what){ return(TRUE)
+  }else if(any(sapply(what, function(cl) inherits(x, cl)))){ return(TRUE)
+  }else{ stop(msg) }
+}
+
 #' Sample ExprsBinary Data
 #' @usage data(array)
 "array"
