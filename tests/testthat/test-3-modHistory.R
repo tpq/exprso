196949c448901857ae4a6bcfaa9359a033fdb138
Thom Quinn <contacttomquinn@gmail.com>
2019-07-14 11:55:13 +1000

    Add test for fsAmalgam (CLR and SLR versions)

--- tests/testthat/test-3-modHistory.R
+++ tests/testthat/test-3-modHistory.R
@@ -2,11 +2,6 @@ library(exprso)
 data(iris)
 e <- exprso(iris[1:100,1:4], iris[1:100,5])
 
-A <- fsPCA(e)
-B <- fsRDA(e)
-C <- fsBalance(e)
-D <- fsAnnot(e, colBy = "y")
-
 test_that("modHistory reconstructs reduced dimensions", {
 
   expect_equal(
@@ -14,23 +9,42 @@ test_that("modHistory reconstructs reduced dimensions", {
     fsPCA(e)
   )
 
+  A <- fsPCA(e)
   expect_equal(
     A@exprs,
     modHistory(e, A)@exprs
   )
 
+  B <- fsRDA(e)
   expect_equal(
     B@exprs,
     modHistory(e, B)@exprs
   )
 
+  C <- fsBalance(e)
   expect_equal(
     C@exprs,
     modHistory(e, C)@exprs
   )
 
+  D <- fsAnnot(e, colBy = "y")
   expect_equal(
     D@exprs,
     modHistory(e, D)@exprs
   )
+
+  if(requireNamespace("amalgam", quietly = TRUE)){
+
+    E <- fsAmalgam(e)
+    expect_equal(
+      E@exprs,
+      modHistory(e, E)@exprs
+    )
+
+    F <- fsAmalgam(e, n.amalgams = 4, asSLR = TRUE)
+    expect_equal(
+      F@exprs,
+      modHistory(e, F)@exprs
+    )
+  }
 })
